8ee27266846d25476207435f0644981e97b09970
HamidMansha1 <hamidmansha115@gmail.com>
2022-11-04 11:52:14 -0700

    run a bulid on travis

---
+++ app/controllers/api/v1/owners_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::OwnersController < ApplicationController
+  before_action :find_owner, only: %i[:only destroy]
+
+  def index
+    @owners = Owner.order(name: :DESC)
+    render json: @owners
+  end
+
+  def create
+    @owner = Owner.new(owner_params)
+    @owner.save!
+    render json: @owner
+  end
+
+  def destroy
+    @owner.delete
+    render json: @owner
+  end
+
+  def update
+    @owner.update(params.permit(:name))
+  end
+
+  private
+
+  def owner_params
+    params.permit(:name)
+  end
+
+  def find_owner
+    @owner = Owner.find(params[:id])
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::API
-  def test_app
-    abc = 1
-  end
 end
